2eb55808c6566add0bcd5132dd115a3d348ec4eb
Rui Zhao <rui.zhao@linksfoundation.com>
2021-04-29 14:23:44 +0200

    add RobotPosition event

---
+++ eu.brain.iot.robot.api/src/main/java/eu/brain/iot/robot/events/RobotPosition.java
@@ -0,0 +1,10 @@
+package eu.brain.iot.robot.events;
+
+import eu.brain.iot.robot.api.RobotCommand;
+
+public class RobotPosition extends RobotCommand {
+
+	public double x;
+	public double y;
+	public double z;
+}
